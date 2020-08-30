daa282766d329c1102267f78bb57ca51f7f344ac
Amílcar Paco <amilcarcpaco@gmail.com>
2020-08-31 01:10:58 +0200

    firebase setuped

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp = {
+    apiKey: "AIzaSyD2XnDwJp16EPocKZQnBeYBucYhNjqhbWs",
+    authDomain: "test-97d16.firebaseapp.com",
+    databaseURL: "https://test-97d16.firebaseio.com",
+    projectId: "test-97d16",
+    storageBucket: "test-97d16.appspot.com",
+    messagingSenderId: "349393722496",
+    appId: "1:349393722496:web:16313b5224278cc76cda65",
+    measurementId: "G-CN9115LP5V"
+  };
+
+export default app;
